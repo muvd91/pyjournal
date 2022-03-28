8e188c93931eb1f5f9b00df686cbfab2f7437e22
David Munoz <david.muv91@gmail.com>
2022-03-27 18:11:00 -0600

    Changed DB connection to fit docker-compose setup.

--- pyjournal/persistence/db.py
+++ pyjournal/persistence/db.py
@@ -3,7 +3,7 @@ from pymongo.errors import ConnectionFailure
 import os
 
 MONGO_DIR = os.path.expandvars('${HOME}/pyjournal/data/mongo_db.conf')
-MONGO_HOST = 'localhost'
+MONGO_HOST = 'db'
 MONGO_DB = 'journal'
 
 database_name = None
@@ -19,14 +19,17 @@ def connect_to_db(db_host=MONGO_HOST, db_name=MONGO_DB):
     client = MongoClient(host=db_host)
     global db_conn
     db_conn = client[database_name]
-    client = MongoClient()
-    try:
-        # The ping command is cheap and does not require auth.
-        client.admin.command('ping')
-    except ConnectionFailure:
-        print("Server not available")
-        exit()
 
+    attempts = 0
+    while attempts < 3:
+        try:
+            client.admin.command('ping')
+            break
+        except ConnectionFailure:
+            attempts += 1
+            if attempts == 3:
+                exit("Couldn't connect to a mongo instance")
+            print("Server not available. Attempting connection again...")
 
 def get_collection(coll):
     global client
